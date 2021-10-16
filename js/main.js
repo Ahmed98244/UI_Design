692d737fbbeadbfb1a2923a648a386747d96fc47
Ahmed98244 <ahmedmartex90@gmail.com>
2021-10-16 23:29:22 +0200

    end edit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+// niceScroll
+$("body").niceScroll({
+  cursorcolor: "#4b73c8",
+  background: "#DDD",
+  cursorwidth: "15px",
+  cursorborderradius: "20px",
+  cursoropacitymin: .4
+});
+
+// funnyText
+
+$(function () {
+  "use strict";
+  $(".welcomee h1").funnyText({
+    speed: 300,
+    color: "#f00",
+    borderColor: "#080",
+  });
+});
